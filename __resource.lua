8d43d3caff20f5aad941927e466c37e0542121b8
Glöckl Nimród <g.nimrod.g@gmail.com>
2018-12-05 22:24:43 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+files{
+    'BlueDiamond/carcols.ymt',
+    'DeepPink/carcols.ymt',
+    'Green/carcols.ymt',
+    'OrangeY/carcols.ymt',
+    'PurpleRc/carcols.ymt',
+    'Red/carcols.ymt',
+    'RoyalBlue/carcols.ymt',
+    'Yellow/carcols.ymt',
+}
+
+
+data_file 'CARCOLS_FILE' 'Red/carcols.ymt'
